274e2807c6d60efc4af8318a50121d53ef3f512c
Luis Estevez <estevezluis1@live.com>
2020-08-12 18:37:31 -0400

    UI-1: responsive quiz

--- src/components/QuestionItem.vue
+++ src/components/QuestionItem.vue
@@ -1,7 +1,16 @@
 <template>
-    <b-card class="shadow-sm rounded mb-4" :title="question">
+    <b-card class="shadow-sm rounded mb-4" :class="[ hasError && question.selectedAnswer === null ? 'border-warning' : borderStyle ]" :title="title">
         <b-form-group class="pl-4">
-          <b-form-radio v-for="(option, index) in options" :key="index" v-model="selected" :name="question" :value="option">{{ option }}</b-form-radio>
+          <b-form-radio
+          v-for="(option, index) in question.options"
+          :key="index"
+          v-model="question.selectedAnswer"
+          :disabled="disabled"
+          :name="question.question"
+          :class="{ 'bg-success rounded': disabled && question.answer === index && !isCorrect }"
+          :value="index"><div :class="{'text-white': disabled && question.answer === index && !isCorrect}">{{ option }}</div>
+          
+          </b-form-radio>
         </b-form-group>
       </b-card>
 </template>
@@ -10,8 +19,35 @@
 export default {
     name: 'QuestionItem',
     props: {
-        question: String,
-        options: Array
+        title: String,
+        question: Object
+    },
+    data() {
+      return { hasError: false, disabled: false, isCorrect: false, borderStyle: '' }
+    },
+    created() {
+      this.question.error = () => {
+        if (this.question.selectedAnswer === null) {
+          this.hasError = true
+          return
+        }
+        this.hasError = false
+      }
+
+      this.question.showResults = (isCorrect) => {
+        if (this.hasError && this.question.selectedAnswer === null) return
+
+        this.disabled = true
+        this.isCorrect = isCorrect
+        if (isCorrect) {
+          this.borderStyle = 'border-success'
+          return
+        }
+        this.borderStyle = 'border-danger'
+
+      }
     }
+
+    
 }
 </script>
--- src/components/QuestionList.vue
+++ src/components/QuestionList.vue
@@ -1,17 +1,47 @@
 <template>
     <div>
+        <div v-show="score !== null">
+            <b-row>
+                <b-col class="text-center">
+                    <p class="lead">Results</p>
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col class="text-center">
+                    {{ scoreDetails() }}
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col class="text-center" :class="[ score >= 70 ? 'text-success' : 'text-danger' ]">
+                    {{ scorePercentage() }}
+                </b-col>
+            </b-row>
+        </div>
         <QuestionItem
          v-for="(question, index) in questions" 
-         :key="index" 
-         :question="question.question" 
-         :options="question.options"
+         :key="index"
+         :ref="index"
+         :question="question"
+         :title="questionTitle(question.question, index)"
+         
         />
+
+    <b-row>
+        <b-col class="text-center">
+            <b-button @click="submitQuiz" :disabled="score !== null" variant="success">Submit</b-button>
+        </b-col>
+    </b-row>
+    <b-row>
+        <b-col class="text-center text-danger">
+            Answer all questions before submitting. Unanswered questions are displayed in yellow.
+        </b-col>
+    </b-row>
     </div>
 </template>
 
 <script>
-import questions from '../assets/questions.json';
-import QuestionItem from './QuestionItem';
+import questions from '../assets/questions.json'
+import QuestionItem from './QuestionItem'
 
 export default {
     name: 'QuestionList',
@@ -20,11 +50,47 @@ export default {
         QuestionItem
     },
     data: () => {
-        return { questions };
+        questions.map((question) => {
+            question.selectedAnswer = null
+        })
+        return {
+            questions,
+            answersCorrect: 0,
+            score: null
+        }
     },
     methods: {
         questionTitle (question, index) {
-            return `${index + 1}. ${question}`;
+            return `${index + 1}. ${question}`
+        },
+        submitQuiz () {
+            let quizCompleted = true
+
+            for (let i = 0; i < questions.length; i++) {
+                if (questions[i].selectedAnswer === null) {
+                    questions[i].error()
+                    quizCompleted = false
+                }
+            }
+            if (quizCompleted) {
+
+                for (let i = 0; i < questions.length; i++) {
+                    const isCorrect = questions[i].selectedAnswer === questions[i].answer
+                    questions[i].showResults(isCorrect)
+
+                    if (isCorrect) {
+                        this.answersCorrect++
+                    }
+                }
+
+                this.score = Math.floor((this.answersCorrect / questions.length) * 100)
+            }
+        },
+        scoreDetails() {
+            return `You got ${this.answersCorrect} of ${questions.length} correct`
+        },
+        scorePercentage() {
+            return `${Math.floor((this.answersCorrect / questions.length) * 100)}%`
         }
     }
 }

cc5ec203d60fe05cec26a2ed83f28f9d01dc2a03
Luis Estevez <estevezluis1@live.com>
2020-08-14 10:24:38 -0400

    utilize reactivity

--- src/components/QuestionItem.vue
+++ src/components/QuestionItem.vue
@@ -1,63 +1,69 @@
 <template>
-    <b-card 
-    class="shadow-sm rounded mb-4" 
-    :class="hasError && question.selectedAnswer === null ? 'border-warning' : `border-${resultStyle}`" 
-    :title="questionTitle"
-
-    >
-        <b-form-group class="pl-4 mb-0">
-          <b-form-radio
-          v-for="(option, index) in question.options"
-          :key="index"
-          v-model="question.selectedAnswer"
-          :disabled="disabled"
-          :name="question.id"
-          :class="{ 'bg-success rounded': disabled && question.answer === index && !isCorrect }"
-          :value="index">
-            <div :class="{'text-white': disabled && question.answer === index && !isCorrect}">{{ option }}</div>
-          
-          </b-form-radio>
-        </b-form-group>
-        <b-row v-show="isCorrect !== null">
-          <b-col><p class="text-right my-0 py-0" :class="`text-${resultStyle}`">{{ result }}</p></b-col>
-        </b-row>
-      </b-card>
+  <b-card
+    class="shadow-sm rounded mb-4"
+    :class="quizHasErrors && selectedAnswer === null ? 'border-warning' : `border-${resultStyle}`"
+    :title="questionTitle">
+    <b-form-group class="pl-4 mb-0">
+      <b-form-radio
+        v-for="(option, index) in question.options"
+        :key="index"
+        :disabled="quizCompleted"
+        v-model="selectedAnswer"
+        :name="question.question"
+        :class="{ 'bg-success rounded': displayCorrectAnswer(index) }"
+        :value="index"
+        @change="$emit('question-answer', $event, questionIndex)"
+      >
+        <div :class="{'text-white': displayCorrectAnswer(index) }">{{ option }}</div>
+      </b-form-radio>
+    </b-form-group>
+    <b-row v-show="quizCompleted">
+      <b-col>
+        <p class="text-right my-0 py-0" :class="`text-${resultStyle}`">{{ result }}</p>
+      </b-col>
+    </b-row>
+  </b-card>
 </template>
 
 <script>
 export default {
-    name: 'QuestionItem',
-    props: {
-        question: Object
+  name: "QuestionItem",
+  props: {
+    question: Object,
+    questionIndex: Number,
+    itemDisabled: Boolean,
+    correctItem: Object,
+    quizHasErrors: Boolean,
+    quizCompleted: Boolean
+  },
+  data() {
+    return {
+      selectedAnswer: null
+    };
+  },
+  computed: {
+    questionTitle() {
+      return `${this.questionIndex + 1}. ${this.question.question}`;
     },
-    data() {
-      return { hasError: false, disabled: false, isCorrect: null, resultStyle: '' }
+    result() {
+      return this.correctItem.correct ? "Correct" : "Wrong";
     },
-    created() {
-      this.question.error = () => {
-        if (this.question.selectedAnswer === null) {
-          this.hasError = true
-          return
-        }
-        this.hasError = false
+    resultStyle() {
+      if (!this.quizCompleted) {
+        return "";
       }
 
-      this.question.showResults = (isCorrect) => {
-        if (this.hasError && this.question.selectedAnswer === null) return
-
-        this.disabled = true
-        this.isCorrect = isCorrect
-        this.resultStyle = isCorrect ? 'success' : 'danger'
-      }
-    },
-    computed: {
-      questionTitle () {
-            return `${this.question.id + 1}. ${this.question.question}`
-      },
-      result () {
-        if (this.isCorrect === null) return ''
-        return this.isCorrect ? 'Correct' : 'Wrong'
-      }
+      return this.correctItem.correct ? "success" : "danger";
     }
-}
+  },
+  methods: {
+    displayCorrectAnswer(index) {
+      return (
+        this.quizCompleted &&
+        !this.correctItem.correct &&
+        this.correctItem.correctAnswer === index
+      );
+    }
+  }
+};
 </script>
--- src/components/QuestionList.vue
+++ src/components/QuestionList.vue
@@ -1,83 +1,94 @@
 <template>
-    <div>
-        <div v-show="score !== null">
-            <b-row>
-                <b-col class="text-center">
-                    <p class="lead"><u>Results</u></p>
-                    <p class="mb-0">{{ scoreDetails }}</p>
-                    <p class="font-weight-bold" :class="score >= 70 ? 'text-success' : 'text-danger'">{{ scorePercentage }}</p>
-                </b-col>
-            </b-row>
-        </div>
-        <QuestionItem
-         v-for="question in questions" 
-         :key="question.id"
-         :question="question"
-         
-        />
+  <div>
+    <div v-show="quizCompleted">
+      <b-row>
+        <b-col class="text-center">
+          <p class="lead">
+            <u>Results</u>
+          </p>
+          <p class="mb-0">{{ scoreDetails }}</p>
+          <p class="font-weight-bold" :class="percentageClass">{{ scorePercentage }}%</p>
+        </b-col>
+      </b-row>
+    </div>
+    <QuestionItem
+      v-for="(question, index) in questions"
+      :key="index"
+      :question="question"
+      :question-index="index"
+      :correct-item="itemScore(question, index)"
+      @question-answer="updateSelectedAnswer"
+      :quiz-has-errors="quizHasErrors"
+      :quiz-completed="quizCompleted"
+    />
 
     <b-row>
-        <b-col class="text-center">
-            <b-button @click="submitQuiz" :disabled="score !== null" variant="success">Submit</b-button>
-            <p class="text-danger">Answer all questions before submitting. Unanswered questions are displayed in yellow.</p>
-        </b-col>
+      <b-col class="text-center">
+        <b-button @click="submitQuiz" variant="success">Submit</b-button>
+        <p
+          class="text-danger"
+        >Answer all questions before submitting. Unanswered questions are displayed in yellow.</p>
+      </b-col>
     </b-row>
-    </div>
+  </div>
 </template>
 
 <script>
-import questions from '../assets/questions.json'
-import QuestionItem from './QuestionItem'
+import questions from "../assets/questions.json";
+import QuestionItem from "./QuestionItem";
+
+const submittedAnswers = Array(questions.length).fill(null);
 
 export default {
-    name: 'QuestionList',
-    props: {},
-    components: {
-        QuestionItem
+  name: "QuestionList",
+  props: {},
+  components: {
+    QuestionItem
+  },
+  data: () => {
+    return {
+      questions,
+      submittedAnswers,
+      quizHasErrors: false,
+      quizCompleted: false
+    };
+  },
+  computed: {
+    correctAnswers() {
+      return this.submittedAnswers.filter((answer, index) => {
+        return answer === this.questions[index].answer;
+      }).length;
     },
-    data: () => {
-        questions.map((question, i) => {
-            question.id = i;
-            question.selectedAnswer = null
-        })
-        return {
-            questions,
-            answersCorrect: 0,
-            score: null
-        }
+    scoreDetails() {
+      return `You got ${this.correctAnswers} of ${questions.length} correct`;
     },
-    computed: {
-        scoreDetails() {
-            return `You got ${this.answersCorrect} of ${questions.length} correct`
-        },
-        scorePercentage() {
-            return `${Math.floor((this.answersCorrect / questions.length) * 100)}%`
-        }
+    scorePercentage() {
+      return Math.floor((this.correctAnswers / questions.length) * 100);
     },
-    methods: {
-        submitQuiz() {
-            let quizCompleted = true
-
-            for (let i = 0; i < questions.length; i++) {
-                if (questions[i].selectedAnswer === null) {
-                    questions[i].error()
-                    quizCompleted = false
-                }
-            }
-            if (quizCompleted) {
-
-                for (let i = 0; i < questions.length; i++) {
-                    const isCorrect = questions[i].selectedAnswer === questions[i].answer
-                    questions[i].showResults(isCorrect)
-
-                    if (isCorrect) {
-                        this.answersCorrect++
-                    }
-                }
-
-                this.score = Math.floor((this.answersCorrect / questions.length) * 100)
-            }
-        }
+    percentageClass() {
+      return this.scorePercentage >= 70 ? "text-success" : "text-danger";
     }
-}
+  },
+  methods: {
+    itemScore(question, index) {
+      const submittedAnswer = this.submittedAnswers[index];
+      return {
+        correct: question.answer === submittedAnswer,
+        correctAnswer: question.answer
+      };
+    },
+    updateSelectedAnswer(answer, index) {
+      this.$set(this.submittedAnswers, index, answer);
+    },
+    submitQuiz() {
+      if (this.submittedAnswers.indexOf(null) === -1) {
+        this.quizCompleted = true;
+        this.quizHasErrors = false;
+        return;
+      }
+      this.quizHasErrors = true;
+      this.quizCompleted = false;
+    }
+  }
+};
 </script>

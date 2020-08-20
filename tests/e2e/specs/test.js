5285c4b30a52383347d2f313e5d80377c1207e39
Luis Estevez <estevezluis1@live.com>
2020-08-20 09:13:28 -0400

    UI-5: modified e2e test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,10 +3,10 @@ import questions from '../../../src/assets/questions.json'
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h2', 'Quiz 1 - HTML / CSS / JS Practice')
+    cy.contains('h1', 'Quiz 1 - HTML / CSS / JS Practice')
     
     questions.forEach((question) => {
-      cy.contains('h4', question.question)
+      cy.get('.card-body > span').contains(question.question)
     })
 
     cy.get('button').click()
